f9ba290eae949b141b8e18cdc57b24b85374aea5
hosseinghafarian <s.h.ghafarian@gmail.com>
2022-01-25 06:18:14 +0330

    Add files via upload

--- get_indices.m
+++ get_indices.m
@@ -2,6 +2,8 @@ function [n_l, Lindex, Uindex, Lind, Uind] = get_indices( data, samples_to_query
 %get_indices This function returns indices of labeled and unlabeled data
 %based on the instance IDs in input arguments
 %   F_to_ind, F_id, samples_to_query_from, initL: all contain dist IDs,
+%   data.F_to_ind: contains original indices of data before splitting to
+%   train and test. 
 %   this function gets labeled and unlabeled indices
 %   this function works in relation to get_distID_fromind
 unlabeled  = setdiff ( samples_to_query_from, initL)';
